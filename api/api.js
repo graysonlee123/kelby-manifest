fca0835f719da089069d254e2a18988574caefd3
graysonlee123 <graysonleegantek@gmail.com>
2020-04-16 12:20:49 -0400

    adds some comments, refactors a bit of code; updates package to use scripts

--- api/api.js
+++ api/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// * @route   GET api/
+// ? @desc    Test route
+// ! @access  Public
 router.get('/', (req, res) => {
   res.json('yep');
 });
--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const path = require('path');
 
+// Execute express process and set port
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Initiate middleware
 app.use(express.json());
 
+// Define base routes
 app.use('/api', require('./api/api'));
 
+// Serve static files for React if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(__dirname, 'client', 'build', 'index.html')
+  );
+}
+
+// Listen for requests
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
