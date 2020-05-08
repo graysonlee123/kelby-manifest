4155338e756af9a0ca55b6838e1d6db10cdb64b0
Grayson Gantek <graysonleegantek@gmail.com>
2020-05-07 20:16:23 -0400

    rewrites aircraft model

--- models/Aircrafts.js
+++ models/Aircrafts.js
@@ -1,47 +1,103 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const AircraftSchema = new mongoose.Schema({
-  gross: {
-    weight: { type: Number },
+  // General Info
+  generalInfo: {
+    tailNum: Number,
+    model: String,
+    maxGasFuel: Number,
+    wingSurfaceArea: Number,
+    thrust: Number,
+    takeOffSpeed: Number,
+    coeficientOfLift: Number,
   },
-  usefulLoad: {
-    weight: { type: Number },
+  // Weight and Balance
+  weightAndBalance: {
+    maxFuel: Number,
+    grossWeight: Number,
+    usefulLoad: Number,
+    emptyWeight: Number,
+    emptyWeightArm: Number,
+    frontSeatArm: Number,
+    middleSeatArm: Number,
+    rearSeatArm: Number,
+    fuelArm: Number,
+    baggageAreaArm: Number,
+    rearBaggageArm: Number,
+    noseBaggageAreaArm: Number,
+    lowCGRAngle: Number,
+    highCG: Number,
   },
-  empty: {
-    weight: { type: Number },
-    arm: { type: Number },
-    moment: { type: Number },
+  // Take Off and Landing Distances
+  takeOffAndLandingDistances: {
+    takeOff: {
+      std: {
+        roll: Number,
+        clear: Number,
+      },
+      plus10: {
+        roll: Number,
+        clear: Number,
+      },
+      plus20: {
+        roll: Number,
+        clear: Number,
+      },
+      plus30: {
+        roll: Number,
+        clear: Number,
+      },
+      plus40: {
+        roll: Number,
+        clear: Number,
+      },
+    },
+    landing: {
+      std: {
+        roll: Number,
+        clear: Number,
+      },
+      plus10: {
+        roll: Number,
+        clear: Number,
+      },
+      plus20: {
+        roll: Number,
+        clear: Number,
+      },
+      plus30: {
+        roll: Number,
+        clear: Number,
+      },
+      plus40: {
+        roll: Number,
+        clear: Number,
+      },
+    },
   },
-  frontSeat: {
-    weight: { type: Number },
-    arm: { type: Number },
-    moment: { type: Number },
-  },
-  middleSeat: {
-    weight: { type: Number },
-    arm: { type: Number },
-    moment: { type: Number },
-  },
-  rearSeat: {
-    weight: { type: Number },
-    arm: { type: Number },
-    moment: { type: Number },
-  },
-  noseBaggage: {
-    weight: { type: Number },
-    arm: { type: Number },
-    moment: { type: Number },
-  },
-  mainBaggage: {
-    weight: { type: Number },
-    arm: { type: Number },
-    moment: { type: Number },
-  },
-  rearBaggage: {
-    weight: { type: Number },
-    arm: { type: Number },
-    moment: { type: Number },
+  // CG Envelope
+  cgEnvelope: {
+    knee: {
+      x: Number,
+      y: Number,
+    },
+    lowerLeft: {
+      x: Number,
+      y: Number,
+    },
+    upperLeftCorner: {
+      x: Number,
+      y: Number,
+    },
+    lowerRight: {
+      x: Number,
+      y: Number,
+    },
+    upperRightCorner: {
+      x: Number,
+      y: Number,
+    },
   },
 });
 
-module.exports = Aircraft = mongoose.model('aircraft', AircraftSchema);
+module.exports = Aircraft = mongoose.model("aircraft", AircraftSchema);
